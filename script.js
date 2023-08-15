8db93d55c93b4a8966eeddbb91ba157d7b837c0e
K4uan1 <142123948+K4uan1@users.noreply.github.com>
2023-08-15 13:31:36 -0300

    primeiro teste

---
+++ script.js
@@ -0,0 +1,30 @@
+const searchInput = document.getElementById('searchInput');
+const scrollButtons = document.querySelectorAll('.scrollButton');
+
+searchInput.addEventListener('input', () => {
+  const query = searchInput.value.trim().toLowerCase();
+
+  scrollButtons.forEach(button => {
+    const buttonNumber = button.getAttribute('data-number');
+    if (query === '' || buttonNumber.includes(query)) {
+      button.style.display = '';
+    } else {
+      button.style.display = 'none';
+    }
+  });
+});
+
+searchInput.addEventListener('blur', () => {
+  if (searchInput.value.trim() === 'block') {
+    showAllButtons();
+  }
+});
+
+function showAllButtons() {
+  scrollButtons.forEach(button => {
+    button.style.display = 'block';
+  });
+}
+
+
+ 
